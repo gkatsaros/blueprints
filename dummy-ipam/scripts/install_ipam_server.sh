385ba19cc3bd71800d712b1d5ffef5f667b069a1
Gregory Katsaros <gregorykatsaros@gregorys-mbp.home>
2018-03-01 22:27:24 +0000

    add background run

--- dummy-ipam/scripts/install_ipam_server.sh
+++ dummy-ipam/scripts/install_ipam_server.sh
@@ -28,4 +28,4 @@ echo "install Dummy Ipam server..."
 
 git clone https://github.com/gkatsaros/scripts.git
 
-python scripts/dummy_ipam/ipam-dummy-server.py 8888 &
+python scripts/dummy_ipam/ipam-dummy-server.py 8888 &> /dev/null &
